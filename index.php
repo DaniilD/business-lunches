b7ca1f7924a58bb897dbab8628bcc32a5e840325
Daniil Domrachev <d.domrachev@bronevik.com>
2022-07-31 17:43:35 +0500

    docker-compose init

--- index.php
+++ index.php
@@ -4,13 +4,25 @@ use App\Controllers\BusinessLunchesController;
 use http\Client\Request;
 use http\Client\Response;
 use Slim\Factory\AppFactory;
+use Slim\Views\Twig;
+use Slim\Views\TwigMiddleware;
 
 require __DIR__ . '/vendor/autoload.php';
 
 $app = AppFactory::create();
 
-$app->get('/', function () {
+// Create Twig
+$twig = Twig::create('public/templates', ['cache' => 'path/to/cache']);
 
+// Add Twig-View Middleware
+$app->add(TwigMiddleware::create($app, $twig));
+
+
+$app->get('/', function ($request, $response, $args) {
+    $view = Twig::fromRequest($request);
+    return $view->render($response, 'profile.html', [
+        'name' => 'this is name'
+    ]);
 });
 
 $app->get('/business-lunches/city/{cityId}', [BusinessLunchesController::class, 'getByCityId']);
